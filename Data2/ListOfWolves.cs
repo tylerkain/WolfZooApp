9e6578048bbf1fd78a8ca6ff770d365b6973480d
tylerkain <68011581+tylerkain@users.noreply.github.com>
2020-10-05 12:50:02 -0400

    Add files via upload

---
+++ Data2/ListOfWolves.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WolfWorld.Models;
+namespace WolfWorld.Data2
+{
+    public class WolfWorldContext : DbContext
+    {
+        public WolfWorldContext(DbContextOptions<WolfWorldContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<ListOfWolves> Wolves { get; set; }
+
+       
+    }
+}
+
