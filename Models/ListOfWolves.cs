200c660a79cc4e75299baee0fe284656c6b1a910
tylerkain <68011581+tylerkain@users.noreply.github.com>
2020-10-05 12:50:57 -0400

    Add files via upload

---
+++ Models/ListOfWolves.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WolfWorld.Models
+{
+    public class ListOfWolves
+    {
+        public int ID { get; set; }
+        public string Subspecies { get; set; }
+        public string Description { get; set; }
+        public string Range { get; set; }
+        public string Order { get; set; }
+        public string Family { get; set; }
+        public string Genus { get; set; }
+        public string ConservationStatus { get; set; }
+
+    }
+}
